e2b5da05d47ee9f7e0f9531760044a4c50543740
aqifbashah <aqifbashah@gmail.com>
2024-03-17 06:29:57 +0800

    minor changes in database connection

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,11 +1,11 @@
 import { Pool } from "pg";
 
 const pool = new Pool({
-  host: "localhost",
-  user: "postgres",
-  password: "admin",
-  database: "pilot-logix",
-  port: "5432",
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
   max: 20,
   idleTimeoutMillis: 30000,
   connectionTimeoutMillis: 2000,
