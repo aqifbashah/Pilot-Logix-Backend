d314f761ee106fd2a574c2a48952e68b92a32c33
aqifbashah <aqifbashah@gmail.com>
2024-03-17 06:55:29 +0800

    add DB_SSL

--- src/database/connection.js
+++ src/database/connection.js
@@ -6,6 +6,7 @@ const pool = new Pool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
+  ssl: process.env.DB_SSL,
   max: 20,
   idleTimeoutMillis: 30000,
   connectionTimeoutMillis: 2000,
