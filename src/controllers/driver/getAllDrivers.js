55298c80c6fb6d3679fd67173f07b5b967ce2924
aqifbashah <aqifbashah@gmail.com>
2024-03-28 02:55:33 +0800

    ...

--- src/controllers/driver/getAllDrivers.js
+++ src/controllers/driver/getAllDrivers.js
@@ -9,13 +9,20 @@ async function getAllDrivers(req, res) {
 
     // Query to get all drivers and their assignments for today
     const query = `
-      SELECT DISTINCT d.id, d.created_at, d.first_name, d.last_name, d.ic_num, d.email, 
+      SELECT d.id, d.created_at, d.first_name, d.last_name, d.ic_num, d.email,
       CASE 
         WHEN '${todayDate}' BETWEEN DATE(a.assignment_date) AND DATE(a.assignment_date) THEN 'assigned'
         ELSE 'available'
       END AS status
       FROM "Drivers" d
-      LEFT JOIN "Assignments" a ON d.id = a.driver1_id OR d.id = a.driver2_id OR d.id = a.driver3_id
+      LEFT JOIN (
+        SELECT DISTINCT driver1_id AS driver_id FROM "Assignments"
+        UNION
+        SELECT DISTINCT driver2_id AS driver_id FROM "Assignments"
+        UNION
+        SELECT DISTINCT driver3_id AS driver_id FROM "Assignments"
+      ) AS assigned_drivers ON d.id = assigned_drivers.driver_id
+      LEFT JOIN "Assignments" a ON assigned_drivers.driver_id = a.driver1_id OR assigned_drivers.driver_id = a.driver2_id OR assigned_drivers.driver_id = a.driver3_id
     `;
 
     // Execute the query
@@ -29,7 +36,7 @@ async function getAllDrivers(req, res) {
       last_name: row.last_name,
       ic_num: row.ic_num,
       email: row.email,
-      status: row.status,
+      status: row.status, // Add the 'status' field to the response
     }));
 
     res.status(200).json(driversData);
