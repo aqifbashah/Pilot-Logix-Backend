01f36c895add9d89a99077362cbdbed2d035cb51
aqifbashah <aqifbashah@gmail.com>
2024-03-28 02:51:20 +0800

    bug fix

--- src/controllers/driver/getAllDrivers.js
+++ src/controllers/driver/getAllDrivers.js
@@ -2,12 +2,14 @@ import pool from "../../database/connection";
 
 async function getAllDrivers(req, res) {
   try {
-    // Get today's date in YYYY-MM-DD format
-    const todayDate = new Date().toISOString().split("T")[0];
+    // Get today's date in GMT+8 timezone (UTC + 8 hours)
+    const todayDate = new Date(new Date().getTime() + 8 * 60 * 60 * 1000)
+      .toISOString()
+      .split("T")[0];
 
     // Query to get all drivers and their assignments for today
     const query = `
-      SELECT d.id, d.created_at, d.first_name, d.last_name, d.ic_num, d.email, 
+      SELECT DISTINCT d.id, d.created_at, d.first_name, d.last_name, d.ic_num, d.email, 
       CASE 
         WHEN '${todayDate}' BETWEEN DATE(a.assignment_date) AND DATE(a.assignment_date) THEN 'assigned'
         ELSE 'available'
@@ -27,7 +29,7 @@ async function getAllDrivers(req, res) {
       last_name: row.last_name,
       ic_num: row.ic_num,
       email: row.email,
-      status: row.status, // Add the 'status' field to the response
+      status: row.status,
     }));
 
     res.status(200).json(driversData);
